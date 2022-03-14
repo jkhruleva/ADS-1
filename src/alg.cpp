e4cdfc23d6b1202d570a07eb331940a32c0fd6bd
jkhruleva <99347813+jkhruleva@users.noreply.github.com>
2022-03-14 17:11:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,82 +1,38 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
-#include <cmath>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
-bool s = true;
-for (int i = 2; i < sqrt(value); i++) {
-if (value%i == 0) {
-s = false;
-break;
+  if (value <=1)
+    return false;
+  for (int q = 2; q <= (value/2); q++) {
+    if ((value % q) == 0)
+      return false;
+  }
+  return true;
 }
-}
-if (s) {
-return true;
-} else {
-return false;
-}
-}
-
-
 uint64_t nPrime(uint64_t n) {
-bool flag = true;
-uint64_t check = 0;
-int number = 0;
-for (int i = 2; flag != false; i++) {
-number = i;
-bool s = true;
-for (int i=2; i <= sqrt(number); i++) {
-if (number%i == 0) {
-s = false;
-break;
-}
-}
-if (s) {
-check++;
-if (check == n) {
-flag = false;
-}
-}
-}
-return number;
+  uint64_t w = 0;
+  uint64_t q = 0;
+  while (q != n) {
+    w++;
+    if (checkPrime(w))
+      q++;
+  }
+  return w;
 }
 
 uint64_t nextPrime(uint64_t value) {
-bool flag = true;
-int num = 0;
-int next = value+1;
-for (next; flag != false; next++) {
-num = next;
-bool s = true;
-for (int i = 2; i <= sqrt(num); i++) {
-if (num%i == 0) {
-s = false;
-break;
+  value++;
+    while (checkPrime(value) != true) {
+      value++;
+    }
+    return value;
 }
-}
-if (s) {
-flag = false;
-}
-}
-return num;
-}
-
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t sum = 0;
-int number = 0;
-for (int i = 2; i < hbound; i++) {
-number = i;
-bool s = true;
-for (int i = 2; i <= sqrt(number); i++) {
-if (number%i == 0) {
-s = false;
-break;
-}
-}
-if (s) {
-sum += number;
-}
-}
-return sum;
+  uint64_t sum = 0;
+    for (uint64_t q = 2; q < hbound; q++) {
+      if (checkPrime(q) == true)
+        sum += q;
+    }
+    return sum;
 }
